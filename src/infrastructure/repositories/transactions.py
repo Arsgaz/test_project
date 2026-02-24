0ee1fb07210f82ef07c99220972bb33187e968c1
Arseniy <arseniy@Arseniys-MacBook-Air.local>
2026-02-24 17:49:45 +0300

    Fix transaction_types

--- src/infrastructure/repositories/transactions.py
+++ src/infrastructure/repositories/transactions.py
@@ -1,18 +1,17 @@
 from sqlalchemy import insert, select 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.infrastructure.db.schema import transactions
-
+from src.infrastructure.db.schema import transactions, transaction_types
 async def create_transaction (
         session: AsyncSession, 
-        type_: str, 
+        transaction_type_id: int, 
         amount: float, 
         date_, 
         category_id: int 
 ) -> dict: 
     stmt = (
         insert(transactions)
-        .values(type = type_, amount = amount, date = date_, category_id = category_id)
+        .values(type = transaction_type_id, amount = amount, date = date_, category_id = category_id)
         .returning (
             transactions.c.id,
             transactions.c.type,
@@ -26,13 +25,20 @@ async def create_transaction (
     return dict(result.mappings().one())
     
 async def get_transactions(session: AsyncSession) -> list[dict]:
-    stmt = select(
-        transactions.c.id,
-            transactions.c.type,
-            transactions.c.amount, 
-            transactions.c.date, 
+    stmt = (
+        select(
+            transactions.c.id,
+            transaction_types.c.code.label("type"),
+            transactions.c.amount,
+            transactions.c.date,
             transactions.c.category_id,
-    ).order_by(transactions.c.id.desc())
+        )
+        .join(
+            transaction_types,
+            transactions.c.transaction_type_id == transaction_types.c.id,
+        )
+        .order_by(transactions.c.id.desc())
+    )
 
     result = await session.execute(stmt)
     return [dict(r) for r in result.mappings().all()]
